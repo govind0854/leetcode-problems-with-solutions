89ddbac9dfdc93ad04ab97cb87806a1cb345cffb
Baratam Govind <24b11ds022@adityauniversity.in>
2026-08-10 20:49:16 +0530

    Time: 23 ms (50.10%) | Memory: 12.5 MB (85.92%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.py
@@ -0,0 +1,12 @@
+from collections import Counter
+class Solution(object):
+    def isAnagram(self, s, t):
+        if Counter(s)==Counter(t):
+            return True
+        else:
+            return False        
+     
+        
+
+
+        
