8840491d0d10cad66f93358e2dac867ebbd2ff4b
Baratam Govind <24b11ds022@adityauniversity.in>
2026-08-12 19:31:32 +0530

    Time: 61 ms (36.30%) | Memory: 13.8 MB (17.21%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
@@ -1,58 +1,27 @@
 class Solution(object):
     def findAnagrams(self, s, p):
-        # d={}
-        # for i in s:
-        #     if i in d:
-        #         d[i]+=1
-        #     else:
-        #         d[i]=1
-        # c={}
-        # for i in p:
-        #     if i in c:
-        #         c[i]+=1
-        #     else:
-        #         c[i]=1
-        # for i in range(len(p)):
-        #     if d[i] in c[i]:
-        #         d[i]+=1
-        #     else:
-        #         d[i]-=1
-        # for i in range(len(p),len(s)):
-        #     if d[i] in c[i]:
-        #         d[i]+=1
-        #     else:
-        #         d[i]-=1
-
-        d={}
-        for i in p:
-            if i in d:
-                d[i]+=1
-            else:
-                d[i]=1
-        left=0
         d1={}
+        for i in p:
+            if i in d1:
+                d1[i]+=1
+            else:
+                d1[i]=1
+        left=0
         ans=[]
+        d2={}
         k=len(p)
         for i in range(len(s)):
-            d1[s[i]]=d1.get(s[i],0)+1
-            if i >= len(p)-1:
-                if d1==d:
+            d2[s[i]]=d2.get(s[i],0)+1
+            if i >=len(p)-1:
+                if d1==d2:
                     ans.append(left)
-                d1[s[left]] -= 1
-                if d1[s[left]]==0:
-                    d1.pop(s[left])
+                d2[s[left]] -=1
+                if d2[s[left]]==0:
+                    d2.pop(s[left])
                 left +=1
-        return ans
+        return ans                
 
 
-
-           
-               
-        
-
-
-
-        
 
         
         
