b99c1a4dc18d78f195fe731f960873bddef836f5
Baratam Govind <24b11ds022@adityauniversity.in>
2026-08-25 16:09:10 +0530

    Time: 0 ms (100.00%) | Memory: 20.6 MB (36.67%) - LeetSync

--- 792-binary-search/binary-search.py
+++ 792-binary-search/binary-search.py
@@ -1,17 +1,15 @@
-class Solution(object):
-    def search(self, nums, target):
-        low=0
-        high=len(nums)-1
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        low = 0
+        high = len(nums) - 1
+
         while low <= high:
-            mid=(low+high)//2
-            if nums[mid]==target:
+            mid = (low+high)//2
+
+            if nums[mid] == target :
                 return mid
             elif nums[mid] > target:
-                high=mid-1
+                high = mid-1
             else:
-                low=mid+1
+                low = mid+1
         return -1
-
-
-        
-        
