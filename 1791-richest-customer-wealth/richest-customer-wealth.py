88a6cd6cc7c22dd7516953f634dfa08e90786e2a
Baratam Govind <24b11ds022@adityauniversity.in>
2026-08-30 18:58:54 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (63.04%) - LeetSync

---
+++ 1791-richest-customer-wealth/richest-customer-wealth.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def maximumWealth(self, accounts):
+        m=0
+        s=0
+        for i in accounts:
+            s=sum(i)
+            if s > m:
+                m=s
+        return m
+
+        
+
+        
+        
