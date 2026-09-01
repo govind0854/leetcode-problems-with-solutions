ad7d4182ab6c42c4db0ce51d59dc465a9d1e092f
Baratam Govind <24b11ds022@adityauniversity.in>
2026-09-01 07:59:31 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (12.16%) - LeetSync

--- 1293-three-consecutive-odds/three-consecutive-odds.py
+++ 1293-three-consecutive-odds/three-consecutive-odds.py
@@ -1,16 +1,6 @@
 class Solution(object):
     def threeConsecutiveOdds(self, arr):
-        m=0
-        c=0
-        for i in arr:
-            if i%2!=0:
-                c +=1
-                m=max(m,c)
-            else:
-                c=0
-        if m >= 3:
-            return True
-        else:
-            return False
-        
-        
+        for i in range(len(arr)-2):
+            if arr[i]%2!=0 and arr[i+1]%2!=0 and arr[i+2]%2!=0:
+                return True
+        return False
